93d3e506e52ac1199c3765ce7b92ac979c85d814
sgavr <sg2295@bath.ac.uk>
2021-03-10 14:50:41 +0200

    Add tests and test data

---
+++ Tests.py
@@ -0,0 +1,48 @@
+import main
+import time
+import numpy as np
+
+
+# Provided testing code, from University of Bath
+
+
+def run_tests(difficulties=None, limit=None):
+
+    difficulties = ['very_easy', 'easy', 'medium', 'hard']
+
+    for difficulty in difficulties:
+        print(f"Testing {difficulty} sudokus")
+
+        sudokus = np.load(f"data/{difficulty}_puzzle.npy")
+        solutions = np.load(f"data/{difficulty}_solution.npy")
+
+        count = 0
+        for i in range(len(sudokus)):
+            sudoku = sudokus[i].copy()
+            print(f"This is {difficulty} sudoku number", i)
+            print(sudoku)
+
+            start_time = time.process_time()
+            your_solution = main.sudoku_solver(sudoku)
+            end_time = time.process_time()
+
+            print(f"This is your solution for {difficulty} sudoku number", i)
+            print(your_solution)
+
+            print("Is your solution correct?")
+            if np.array_equal(your_solution, solutions[i]):
+                print("Yes! Correct solution.")
+                count += 1
+            else:
+                print("No, the correct solution is: --------------------------------------------------------------")
+                print(solutions[i])
+
+            print("This sudoku took", end_time-start_time, "seconds to solve.\n")
+
+        print(f"{count}/{len(sudokus)} {difficulty} sudokus correct")
+        if count < len(sudokus):
+            break
+
+
+if __name__ == "__main__":
+    run_tests()
