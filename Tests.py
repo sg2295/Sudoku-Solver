3a29449c29dd0ca987005015093a6a2b23b94c34
sgavr <sg2295@bath.ac.uk>
2021-03-10 23:18:33 +0200

    Adapt for hard tests

--- Tests.py
+++ Tests.py
@@ -25,7 +25,7 @@ def run_tests(difficulties=None):
             start_time = time.process_time()
             your_solution = main.sudoku_solver(sudoku)
             end_time = time.process_time()
-            return
+
             print(f"This is your solution for {difficulty} sudoku number", i)
             print(your_solution)
 
@@ -36,14 +36,16 @@ def run_tests(difficulties=None):
             else:
                 print("No, the correct solution is: --------------------------------------------------------------")
                 print(solutions[i])
+                break # TODO REMOVE
 
             print("This sudoku took", end_time - start_time, "seconds to solve.\n")
-
+            return # TODO REMOVE
         print(f"{count}/{len(sudokus)} {difficulty} sudokus correct")
         if count < len(sudokus):
             break
 
 
 if __name__ == "__main__":
-    d = ["very_easy"]
+    d = ["hard"]
     run_tests(d)
+    # run_tests()
